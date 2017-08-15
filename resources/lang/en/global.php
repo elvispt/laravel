5b56ed051db22f11f7c0bba1ea33ef5d917f258e
elvispt <elvispt@gmail.com>
2017-08-15 13:01:46 +0100

    Add global language file

---
+++ resources/lang/en/global.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Global Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used globally in the application.
+    |
+    */
+
+    'app_title' => 'Laravel-React',
+
+];
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,14 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel</title>
-    </head>
-    <body>
-      <h1>Laravel-React</h1>
-    </body>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>@lang('global.app_title')</title>
+  <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+</head>
+<body>
+  <h1>@lang('global.app_title')</h1>
+
+</body>
 </html>
