7997877f8ed49f8b965b6e52c1a590716cfb1e6c
elvispt <elvispt@gmail.com>
2017-09-02 18:56:25 +0100

    Update seed factory to avoid tag names collisions
    Generating tags was causing issues on DB since it was marked as unique in db. We now check if it exists before adding to table.
    Now setting timestamp fields on seed factory.

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -14,28 +14,47 @@ use App\Models\User;
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 $factory->define(User::class, function (Faker\Generator $faker) {
+    $created_at = $faker->dateTimeThisDecade();
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'password' => bcrypt('secret'),
         'remember_token' => str_random(10),
+        'updated_at' => $faker->dateTimeBetween($created_at),
+        'created_at' => $created_at,
     ];
 });
 
 $factory->define(App\Models\Note::class, function (Faker\Generator $faker) {
+    $created_at = $faker->dateTimeThisDecade();
     return [
         'user_id' => function () {
             return (new User())->inRandomOrder()->first()->id;
         },
         'note' => $faker->text(400),
+        'updated_at' => $faker->dateTimeBetween($created_at),
+        'created_at' => $created_at,
     ];
 });
 
 $factory->define(App\Models\Tag::class, function (Faker\Generator $faker) {
+    $created_at = $faker->dateTimeThisDecade();
+    $generated_tag = $faker->unique()->word;
+    $running = true;
+    while ($running) {
+        $exists = (new \App\Models\Tag())->where('tag', $generated_tag)->count('id') > 0;
+        if ($exists) {
+            $generated_tag = $faker->unique()->word;
+        } else {
+            $running = false;
+        }
+    }
     return [
         'user_id' => function () {
             return (new User())->inRandomOrder()->first()->id;
         },
-        'tag' => $faker->unique()->word,
+        'tag' => $generated_tag,
+        'updated_at' => $faker->dateTimeBetween($created_at),
+        'created_at' => $created_at,
     ];
 });
