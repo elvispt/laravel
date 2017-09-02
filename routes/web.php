357217a45080cfede3cbfb72341ae57ebce9c728
elvispt <elvispt@gmail.com>
2017-09-02 17:15:04 +0100

    Commit example code for obtaining notes

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,22 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    $userModel = new \App\Models\User();
+    $user = $userModel->with([
+        'notes',
+        'tags'
+    ])->first();
+    return $user;
+});
+
+Route::get('/notes', function () {
+    $note = new \App\Models\Note();
+    return $note->with(['tags'])->first();
+});
+
+Route::get('/tags', function () {
+    return \App\Models\Tag::all();
 });
